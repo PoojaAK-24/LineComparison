494ba33c8651cc781a3682b25ebb4b424cc88fc3
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-05 11:34:59 +0530

    Welcome To LineComparison

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+            Console.WriteLine("Welcome to the Line Comparison Program!");
+            //UC1_CheckLength.getCartesian();
+            //UC2_TwoLinesAreEqual.getCartesian();
+            UC3_EqualGreaterLesser.getCartesian();
+        }
+        }
+    }
